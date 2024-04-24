61eff20c5d4e68aaf3ec8e848c23f6a2f83959e1
App317 <101300095+App317@users.noreply.github.com>
2024-04-24 19:33:50 -0400

    Github Pages

---
+++ src/components/Main.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect } from 'react';
+import Card from './Card';
+import PokeInfo from './PokeInfo';
+import { useState } from 'react';
+import axios from 'axios';
+
+const Main = () => {
+  const [pokeData, setPokeData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon/');
+  const [nextUrl, setNextUrl] = useState();
+  const [prevUrl, setPrevUrl] = useState();
+  const [pokeDex, setPokeDex] = useState();
+
+  const pokeFun = async () => {
+    setLoading(true);
+    const response = await axios.get(url);
+    setNextUrl(response.data.next);
+    setPrevUrl(response.data.previous);
+    getPokemon(response.data.results);
+    setLoading(false);
+    //console.log(pokeData);
+  };
+  //duplication error most likely occuring somehow in this code
+  const getPokemon = async (response) => {
+    response.map(async (item) => {
+      const result = await axios.get(item.url);
+      setPokeData((state) => {
+        state = [...state, result.data];
+        state.sort((a, b) => (a.id > b.id ? 1 : -1));
+        return state;
+      });
+    });
+  };
+
+  useEffect(() => {
+    pokeFun();
+  }, [url]);
+  return (
+    <>
+      <div className="container">
+        <div className="left-content">
+          <Card
+            pokemon={pokeData}
+            loading={loading}
+            infoPokemon={(poke) => setPokeDex(poke)}
+          />
+
+          <div className="btn-group">
+            {prevUrl && (
+              <button
+                onClick={() => {
+                  setPokeData([]);
+                  setUrl(prevUrl);
+                }}
+              >
+                Previous
+              </button>
+            )}
+
+            {nextUrl && (
+              <button
+                onClick={() => {
+                  setPokeData([]);
+                  setUrl(nextUrl);
+                }}
+              >
+                Next
+              </button>
+            )}
+          </div>
+        </div>
+        <div className="right-content">
+          <PokeInfo data={pokeDex} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
